6e2b940bb54a1f3bdbb11263811310fed19725c5
BS-Rud7a <aas2424002@sicsr.ac.in>
2026-08-25 15:18:54 +0530

    More testing with decision making

--- Brain/brain.py
+++ Brain/brain.py
@@ -1,9 +1,23 @@
 def mitra_response(message):
-    return "Hey! I heard you say: " + message
+
+    message = message.lower()
+
+    if "hello" in message or "hi" in message:
+        return "Hey! It's good to hear from you! 😊"
+
+    elif "how are you" in message:
+        return "I'm doing great! Thanks for asking! How are you?"
+
+    elif "bye" in message:
+        return "See you later! 👋"
+    elif "what is your name" in message:
+        return "I am Mitra, your mitra  "
+    else:
+        return "Hmm, tell me more about that."
 
 
 user_message = input("You: ")
 
 response = mitra_response(user_message)
 
-print("Mitra:", response)
+print("Mitra:", response) 
